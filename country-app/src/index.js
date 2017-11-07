657a4304f98cef9b9a18c81884475873f8b1b657
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-11-07 21:10:12 +0100

    Update code

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/index';
+import routes from './routes';
+import './country.css';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+        <Router history={hashHistory} routes={routes} />
     </Provider>,
     document.getElementById('root')
 );
